cc1ce262c9baf93aa15c8d7de075133dd1bfaf21
igor04091968 <igor04091968@gmail.com>
2025-12-14 21:38:05 +0300

    Update default web and sub ports to 443 and 80

--- src/service/setting.go
+++ src/service/setting.go
@@ -44,7 +44,7 @@ var defaultConfig = `{
 var defaultValueMap = map[string]string{
 	"webListen":     "",
 	"webDomain":     "",
-	"webPort":       "80",
+	"webPort":       "443",
 	"secret":        common.Random(32),
 	"webCertFile":   "",
 	"webKeyFile":    "",
@@ -54,7 +54,7 @@ var defaultValueMap = map[string]string{
 	"trafficAge":    "30",
 	"timeLocation":  "Asia/Tehran",
 	"subListen":     "",
-	"subPort":       "443",
+	"subPort":       "80",
 	"subPath":       "/sub/",
 	"subDomain":     "",
 	"subCertFile":   "",
